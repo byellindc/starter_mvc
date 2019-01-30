90cff1780d1a5d43da3f77885126d71ebc442096
Ben Yellin <byellindc@gmail.com>
2019-01-30 15:11:00 -0500

    fix seed duplication

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
+Team.delete_all
 team1 = Team.create(name: "Rockets", color: "DarkOrchid", sport: "Lacrosse")
 team2 = Team.create(name: "Slime Eels", color: "YellowGreen", sport: "Lacrosse")
 team3 = Team.create(name: "Bad Muffins", color: "BlanchedAlmond", sport: "Cooking")
 
+Player.delete_all
 player1 = Player.create(name: "Bob", height: 70, sex: "M", nickname:"Thababo", skill: "Friendship", rating: 28, team: team1)
 player2 = Player.create(name: "Robb", height: 70, sex: "M", nickname:"Thababo", skill: "Friendship", rating: 28, team: team1)
 player3 = Player.create(name: "Clobb", height: 70, sex: "M", nickname:"Thababo", skill: "Friendship", rating: 28, team: team1)
